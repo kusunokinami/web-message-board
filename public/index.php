cdabeddea133fa638cdd88c0b456c1bb16cfb5f0
Kusu <tosiki2001.39@gmail.com>
2026-09-20 21:10:15 +0900

    dockerで環境を構築

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ひとこと掲示板</title>
+</head>
+
+<body>
+    <div class="page-cover">
+        <h1>ひとこと掲示板</h1>
+        <div class="form-cover">
+            <!-- 投稿内容入力フォーム -->
+        </div>
+        <div class="message-list-cover">
+            <!-- この中に投稿された内容のリストを表示 -->
+        </div>
+
+        <hr class="page-divider" />
+    </div>
+</body>
+
+</html>
